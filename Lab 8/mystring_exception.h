bedd54dcaac2a93ea21ab6cc40b4860ebf67b55e
Sugarcane <jzou89260@gmail.com>
2020-12-06 17:35:57 -0500

    add test file and .h(design) file

---
+++ Lab 8/mystring_exception.h
@@ -0,0 +1,21 @@
+#ifndef MYSTRING_EXCEPTION
+#define MYSTRING_EXCEPTION
+
+#include <string>
+
+namespace mystring
+{
+
+    class string_Exception
+    {
+    public:
+        string_Exception(string what) : what(what) {}
+        string getWhat() { return what; }
+
+    private:
+        string what;
+    };
+
+} // namespace mystring
+
+#endif
